a050d0b10453a3145e48bd94cc141dc2da16c04f
oumatoulacen <oumatoulacen@gmail.com>
2023-10-05 23:26:46 +0100

    commit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,19 +2,11 @@
 '''a Fabric script that generates a .tgz archive from the contents
 of the web_static folder '''
 
-from fabric.api import local, run, put
-from datetime import datetime
-import os
+from fabric.api import local
+from fabric.decorators import task
 
+@task
 def do_pack():
     ''' archive from the contents of the web_static'''
-    env.hosts = ['100.26.167.149	','100.25.163.174']
-    env.user = ubuntu
-    now = datetime.now()
-    now = now.strftime("%Y%m%d%H%M%S")
-
-    os.makedirs("versions")
-    archive_name = "web_static_{}.tgz".format(now)
-    local("tar -cvzf versions/{} web_static".format(archive_name))
-    return "versions/{}".format(archive_name)
+    local("mkdir version; tar -cvzf versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/")
 
