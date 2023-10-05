5945f8fe210b1ebe5831f3a134d52b42d879d8a3
oumatoulacen <oumatoulacen@gmail.com>
2023-10-05 23:21:25 +0100

    commit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,8 +3,18 @@
 of the web_static folder '''
 
 from fabric.api import local, run, put
+from datetime import datetime
+import os
 
 def do_pack():
     ''' archive from the contents of the web_static'''
-    local('ls')
+    env.hosts = ['100.26.167.149	','100.25.163.174']
+    env.user = ubuntu
+    now = datetime.now()
+    now = now.strftime("%Y%m%d%H%M%S")
+
+    os.makedirs("versions")
+    archive_name = "web_static_{}.tgz".format(now)
+    local("tar -cvzf versions/{} web_static".format(archive_name))
+    return "versions/{}".format(archive_name)
 
