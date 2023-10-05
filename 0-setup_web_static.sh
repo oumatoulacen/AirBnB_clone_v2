6b385b1cb48e51f6da3658d88f6203699f32103c
oumatoulacen <oumatoulacen@gmail.com>
2023-10-05 15:48:48 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,55 +1,23 @@
 #!/bin/bash
 # a Bash script that sets up your web servers for the deployment of web_static.
 
-# Install Nginx if not already installed apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 
-# Create necessary directories
 web_static_dir="/data/web_static/releases/test"
 shared_dir="/data/web_static/shared"
 
-mkdir -p /data/web_static/{releases/test,shared}
-touch "$web_static_dir/index.html"
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 echo "Hello, this is a test HTML file." > "$web_static_dir/index.html"
-
 # Create or recreate symbolic link
 if [ -L /data/web_static/current ]; then
     rm /data/web_static/current
 fi
 ln -sf "$web_static_dir" /data/web_static/current
-
-# Set ownership recursively
-chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-config_text=$(cat <<EOL
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location /hbnb_static/ {
-        alias $shared_dir;
-        expires 30d;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    error_page 500 502 503 504 /custom_50x.html;
-    location = /custom_50x.html {
-        root /usr/share/nginx/html;
-        internal;
-    }
-}
-EOL
-)
-
-echo "$config_text" > "$config_file"
-
+sudo chown -R ubuntu:ubuntu /data/
+old="server_name _;"
+new="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "s|$old|$new|" /etc/nginx/sites-enabled/default
 sudo service nginx restart
-
 echo "Web server setup completed."
