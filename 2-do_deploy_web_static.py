11994bb8fa8d2c4f306460e572ea02c6a5934d2d
oumatoulacen <oumatoulacen@gmail.com>
2023-10-06 00:44:23 +0100

    distributes an archive to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,13 +14,9 @@ def do_deploy(archive_path):
         without_ext = archive_path.split("/")[-1].split(".")[0]
         put(archive_path, "/tmp")
         run("mkdir -p /data/web_static/releases/" + without_ext)
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/\
-{}".format(with_ext, without_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}\
+                ".format(with_ext, without_ext))
         run("rm /tmp/{}".format(with_ext))
-        run("mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}".format(without_ext, without_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static\
-".format(without_ext))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ \
 /data/web_static/current".format(without_ext))
