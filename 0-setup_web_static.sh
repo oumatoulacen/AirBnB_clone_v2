150238e43c158e9e74a9f851215edba6624e824d
oumatoulacen <oumatoulacen@gmail.com>
2023-10-05 21:14:25 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # a Bash script that sets up your web servers for the deployment of web_static.
 
 sudo apt-get update
@@ -16,4 +16,3 @@ old="server_name _;"
 new="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "s|$old|$new|" /etc/nginx/sites-enabled/default
 sudo service nginx restart
-echo "Web server setup completed."
