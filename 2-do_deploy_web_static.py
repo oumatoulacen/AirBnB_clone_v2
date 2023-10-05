e40475a0a7bbadfbb5f33e7a2bef7bc26495e900
oumatoulacen <oumatoulacen@gmail.com>
2023-10-06 00:36:10 +0100

    distributes an archive to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
-'''a Fabric script that generates a .tgz archive'''
-from fabric.api import local, run, put
+""" Fabric script that generates a .tgz archive """
 from fabric.decorators import task
+from fabric.api import *
 
-
-@task
-def do_pack():
-    ''' archive from the contents of the web_static'''
-    local("mkdir versions; tar -cvzf versions/web_static_$(date \
-            +%Y%m%d%H%M%S).tgz web_static/")
-
-
-env.host = ['100.25.163.174', '100.26.167.149']
+env.hosts = ["100.27.4.102", "54.165.197.71"]
 
 
 @task
 def do_deploy(archive_path):
-    '''distributes an archive to your web servers'''
+    """Fabric script that distributes an archive to web servers"""
     try:
-        name_with_ext = archive_path.split("/")[-1]
-        name_without_ext = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-            name_with_ext, name_without_ext))
-        run('rm -rf /tmp/{}'.format(name_with_ext))
-        run('rm /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} \
-                /data/web_static/current'.format(name_without_ext))
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp")
+        run("mkdir -p /data/web_static/releases/" + without_ext)
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/\
+{}".format(with_ext, without_ext))
+        run("rm /tmp/{}".format(with_ext))
+        run("mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}".format(without_ext, without_ext))
+        run("rm -rf /data/web_static/releases/{}/web_static\
+".format(without_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ \
+/data/web_static/current".format(without_ext))
         return True
     except Exception:
         return False
+
+
+@task
+def do_pack():
+    """generates a .tgz archive from web_static"""
+    local(
+        "mkdir versions ; tar -cvzf \
+versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/"
+    )
