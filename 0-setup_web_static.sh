f15b84e142e607e23a74704abfc38a42f4406b2f
oumatoulacen <oumatoulacen@gmail.com>
2023-10-05 21:22:25 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,10 @@
 # a Bash script that sets up your web servers for the deployment of web_static.
 sudo apt-get update
 sudo apt-get -y install nginx
-web_static_dir="/data/web_static/releases/test"
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "Hello, this is a test HTML file." > "$web_static_dir/index.html"
-ln -sf "$web_static_dir" /data/web_static/current
+echo "Hello, this is a test HTML file." > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 old="server_name _;"
 new="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
