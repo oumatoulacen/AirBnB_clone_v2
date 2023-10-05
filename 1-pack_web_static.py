f3484ae4db6e1132894c1bea9b1da5883c32685d
oumatoulacen <oumatoulacen@gmail.com>
2023-10-05 23:32:29 +0100

    commit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''a Fabric script that generates a .tgz archive from the contents
 of the web_static folder '''
-
 from fabric.api import local
 from fabric.decorators import task
+
 
 @task
 def do_pack():
     ''' archive from the contents of the web_static'''
-    local("mkdir version; tar -cvzf versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/")
-
+    local("mkdir versions; tar -cvzf versions/web_static_$(date \
+            +%Y%m%d%H%M%S).tgz web_static/")
