1c4811cb6fa320ab20e6f3f4d3e21c76cd507f90
oumatoulacen <oumatoulacen@gmail.com>
2023-10-20 13:25:14 +0100

    start app

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''Write a script that starts a Flask web application'''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace("_", " ")
+    return "C " + text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyt_text(text="is_cool"):
+    text = text.replace("_", " ")
+    return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
