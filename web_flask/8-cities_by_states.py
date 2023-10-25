16b216b891c3a35ce608070f3066840ee49ab1b4
oumatoulacen <oumatoulacen@gmail.com>
2023-10-25 07:18:21 +0100

    change style

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application """
+"""script that starts a Flask web application"""
 from models import storage
 from flask import Flask, render_template
 from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """ list all states"""
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exception):
-    ''''closes the connection'''
+def closedb(stor):
+    """Closes db session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/cities_by_states', strict_slashes=False)
+def states_template():
+    """function that route /states_list"""
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
