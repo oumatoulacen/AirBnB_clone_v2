9041910bb97c3d7c477bf3a12a099c3bbaed6a4e
oumatoulacen <oumatoulacen@gmail.com>
2023-10-25 06:25:26 +0100

    change style

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
-
-from flask import Flask, render_template
 from models import storage
+from flask import Flask, render_template
 from models.state import State
-
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes = False)
 def states_list():
-    """ list all states"""
-    states = storage.all(State)
-    states = dict(sorted(states.items(), key=lambda item: item[1].name))
+    """ list all states"""    
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
     return render_template("7-states_list.html", states=states)
 
 
@@ -23,4 +21,5 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
+    storage.reload()
     app.run(host="0.0.0.0", port=5000)
